3f655f44ee7f4d01b104be0e96994d83eb85e52c
Sadia0920 <sadiaafrin0920@gmail.com>
2025-02-21 01:25:55 +0600

    added a login and register form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,99 @@
-function Login() {
+import React, { useContext, useState } from 'react'
+import { AuthContext } from '../provider/AuthProvider';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import Swal from 'sweetalert2';
 
-    return (
-      <>
-      <div>Login</div>
-      </>
-    )
+export default function Login() {
+  const {signInUser,signInWithGoogle,setUser} = useContext(AuthContext);
+  const navigate = useNavigate()
+  const [showPassword,setShowPassword]=useState(false)
+
+  const handleSignInUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const user = {email,password}
+    // console.log(user);
+
+    // SignInUser
+    signInUser(email,password)
+    .then(result => {
+      // console.log(result.user)
+      setUser(result.user)
+      Swal.fire({
+        title: 'Success',
+        text: 'Login successfully',
+        icon: 'success',
+        confirmButtonText: 'Done'
+      })
+      navigate('/')
+      form.reset()
+    })
+    .catch(error => {
+      // console.log(error.message)
+      setUser(null)
+      Swal.fire({
+        title: 'Error',
+        text: error.message,
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      })
+    })
+
   }
-  
-  export default Login
+  const handleGoogleLogin = () => {
+    signInWithGoogle()
+    .then(result => {
+      // console.log(result.user)
+      setUser(result.user)
+      navigate('/')
+    })
+    .catch(error => {
+      // console.log(error)
+      setUser(null)
+    })
+  }
+  return (
+    <div className='my-7 w-11/12 lg:w-6/12 mx-auto'>
+      <div className="hero bg-base-200 rounded-xl py-5">
+  <div className="hero-content flex-col w-full md:w-7/12 lg:w-10/12 mx-auto">
+    <div className="text-center">
+      <h1 className="text-3xl font-bold text-gray-800">Login now!</h1>
+    </div>
+    <div className="card bg-base-100 w-full shadow-2xl">
+      <form onSubmit={handleSignInUser} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="email" name='email' className="input input-bordered " required />
+        </div>
+        <div className="form-control relative">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type={showPassword?'text':'password'} placeholder="password" name='password' className="input input-bordered" required />
+          <a onClick={()=>setShowPassword(!showPassword)} className="btn btn-xs text-lg absolute mt-12 ml-[250px] lg:ml-[380px]">{showPassword?<FaEyeSlash></FaEyeSlash>:<FaEye></FaEye>}</a>
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn bg-[#10B981] text-white">Login</button>
+        </div>
+      </form>
+      
+      <div className="mx-auto">
+          <button onClick={handleGoogleLogin} className="btn px-[65px] lg:px-[144px] mb-4 bg-[#4A90E2] text-white">
+            <i className="fa-brands fa-google"></i>
+            Login With Google</button>
+      </div>
+        <p className='font-semibold text-center px-5 mb-6'>Don't have an account? please <Link className='border-b-2 border-[#0a3d62] text-[#0a3d62]' to='/register'>Register</Link></p>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,124 @@
-function Register() {
+import React, { useContext, useState } from 'react'
+import { AuthContext } from '../provider/AuthProvider';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import Swal from 'sweetalert2';
 
-    return (
-      <>
-      <div>register</div>
-      </>
-    )
+export default function Register() {
+
+  const {createUser,setUser,updateUserInfo} = useContext(AuthContext);
+  const navigate = useNavigate()
+  const [showPassword,setShowPassword]=useState(false)
+  const [errorMessage,setErrorMessage]=useState('')
+
+  const handleCreateUser = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const photo = form.photo.value;
+    const password = form.password.value;
+
+    const newUser = {name,photo,email,password}
+    // console.log(newUser);
+
+    setErrorMessage('')
+
+     const regex = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+
+     if(!regex.test(password)){
+      setErrorMessage('Please give a valid password with at lease one Uppercase, one Lowercase and length must be 6 character or more.')
+      Swal.fire({
+        title: 'Error',
+        text: errorMessage,
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      })
+      return;
+    }
+
+    // CreateUser
+    createUser(email,password)
+    .then(result => {
+      setUser(result.user)
+      form.reset();
+      navigate('/')
+      Swal.fire({
+        title: 'Success',
+        text: 'Register Successfully',
+        icon: 'success',
+        confirmButtonText: 'Done'
+      })
+    // UpdateUser
+    const profile = {
+      displayName: name,
+      photoURL: photo
+    }
+    updateUserInfo(profile)
+    .then((res)=>{
+      // console.log(res.user)
+    })
+    .catch(error => {
+      setErrorMessage(error.message)
+    })
+    })
+    .catch(error => {
+      // console.log(error.message)
+      setErrorMessage(error.message)
+      setUser(null)
+      Swal.fire({
+        title: 'Error',
+        text: error.message,
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      })
+    })
+
+  }
+  return (
+    <div className='my-7 w-11/12 lg:w-6/12 mx-auto'>
+      <div className="hero bg-base-200 rounded-xl py-5">
+  <div className="hero-content flex-col w-full md:w-7/12 lg:w-10/12 mx-auto">
+    <div className="text-center">
+      <h1 className="text-3xl font-bold text-gray-800">Register now!</h1>
+    </div>
+    <div className="card bg-base-100 w-full shadow-2xl">
+      <form onSubmit={handleCreateUser} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input type="text" placeholder="name" name='name' className="input input-bordered " required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Photo</span>
+          </label>
+          <input type="text" placeholder="photo URL" name='photo' className="input input-bordered " required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="email" name='email' className="input input-bordered " required />
+        </div>
+        <div className="form-control relative">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type={showPassword?'text':'password'}  placeholder="password" name='password' className="input input-bordered" required />
+          <a onClick={()=>setShowPassword(!showPassword)} className="btn btn-xs text-lg absolute mt-12 ml-[250px] lg:ml-[380px]">{showPassword?<FaEyeSlash></FaEyeSlash>:<FaEye></FaEye>}</a>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn bg-[#10B981] text-white">Register</button>
+        </div>
+      </form>
+      
+      <p className='font-semibold text-center px-5 mb-6'>Already have an account? please <Link className='border-b-2 border-[#0a3d62] text-[#0a3d62]' to='/login'>Login</Link></p>
+    </div>
+  </div>
+</div>
+    </div>
+  )
   }
   
-  export default Register
