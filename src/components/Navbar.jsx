ed22d79cd58829be609cdf326cdafc895ee76887
Sadia0920 <sadiaafrin0920@gmail.com>
2025-02-21 00:55:41 +0600

    install a firebase

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 function Navbar() {
 
     return (
-      <div>
-       <div className="navbar bg-base-100">
+      <div  className="bg-[#10B981]">
+       <div className="navbar w-10/12 mx-auto">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -33,7 +33,7 @@ function Navbar() {
         <li><a>Item 3</a></li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl"><i className="fa-solid fa-list-check"></i>Task-Board</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
